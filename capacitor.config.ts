11ea2ea6489edbd26398350e72425fa928f52b2b
Alejandro_g27 <cesaralejandrog67@gmail.com>
2026-05-25 20:47:30 -0500

    Primer commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.alejandro.nexura',
+  appName: 'Nexura',
+  webDir: 'www'
+};
+
+export default config;
---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { 
+  IonHeader, 
+  IonToolbar, 
+  IonTitle, 
+  IonContent, 
+  IonItem, 
+  IonInput, 
+  IonButton 
+} from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss'],
+  standalone: true,
+  imports: [
+    IonHeader, 
+    IonToolbar, 
+    IonTitle, 
+    IonContent, 
+    IonItem, 
+    IonInput, 
+    IonButton
+  ]
+})
+export class Tab2Page {
+  constructor() {}
+}
